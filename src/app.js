97d8640c5f1048f8d69c32d6b5bbe4e3b58cf1ef
Pablo Martin <paumf00@gmail.com>
2024-09-17 23:25:00 +0200

    js_v0.2.1 first

--- src/app.js
+++ src/app.js
@@ -5,188 +5,352 @@ const consoleMPDS = new Console();
 playMastermind();
 
 function playMastermind() {
+    const continueDialog = initYesNoDialog("Do you want to continue?");
+    /*console.log(typeof ["y", "n"])
+    for (let property in continueDialog){
+        console.log(property);
+        console.log(typeof continueDialog[property]);
+        consoleMPDS.writeln();
+    }
+        */
     do {
-        playGame();
-    } while (isResume());
+        const newGame = initGame();        
+        newGame.play();
+        continueDialog.ask();
+    } while (continueDialog.isAffirmative());
+}
 
-    function playGame() {
-        const game = initGame();      
-        showTitle(game.TITLE);  
-        showBoard(game.board);        
-        do {
-            const proposed = askProposed(game.rules);
-            addAttempt(game.board, proposed);
-            showBoard(game.board);            
-        } while (!isWinner(game.board) && hasMoreAttempts(game));
-        consoleMPDS.writeln(`You've ${isWinner(game.board) ? `won!!! ;-)` : `lost!!! :-(`}`);
+function initGame() {
+    const validator = initCombinationValidator();
+    const game = {
+        validator: validator,
+        board: initBoard(),
+        player: initPlayer(validator),
 
-        // secret & proposed como Strings
-        // black/white counts renombrar como blacks/whites
-
-        function initGame() {            
-            const game = {
-                TITLE: `MASTERMIND`, 
-                rules: {
-                    COLORS: ['r', 'g', 'y', 'b', 'm', 'c'], 
-                    COMBINATION_LENGTH: 4, 
-                    MAX_ATTEMPTS: 10 
-                },
-                board: {        
-                    attempts: []   
-                },
-            };        
-            generateSecretRandomly(game);
-            return game;
-
-            function generateSecretRandomly({rules, board}) {    
-                board.secret = '';        
-                for (let i = 0; i < rules.COMBINATION_LENGTH; i++) {
-                    let color;
-                    do {
-                        let indexColor = parseInt(Math.random() * rules.COLORS.length);
-                        color = rules.COLORS[indexColor];
-                    } while (isValuePresent(board.secret, color));
-                    board.secret += color;
-                }
-            }
-        }
-
-        function showTitle(title) {
-            consoleMPDS.writeln(`----- ${title} -----`);
-        }
-            
-        function showBoard({ attempts, secret }) {
-            consoleMPDS.writeln(`\n${attempts.length} attempt(s):`)
-            showSecret(secret);
-            for (let i = 0; i < attempts.length; i++) {
-                showAttempt(attempts[i]);
-            }
-
-            function showSecret(secret) {
-                const HIDDEN_CHAR = '*';
-                let msg = '';
-                for (let i = 0; i < secret.length; i++) {
-                    msg += HIDDEN_CHAR;
-                }
-                consoleMPDS.writeln(msg);
-            }
-
-            function showAttempt(attempt) {
-                let msg = attempt.proposed;
-                msg += ` --> ${attempt.blacks} blacks and ${attempt.whites} whites`;
-                consoleMPDS.writeln(msg);
-            }
-        }
-
-        function askProposed(rules) {
-            let proposed;
-            let error;
+        play: function () {
+            showWelcome();
+            game.board.secret = generateSecretRandomly(game);
+            game.board.show();
             do {
-                proposed = consoleMPDS.readString(`Propose a combination:`);
-                const errorMsg = getErrorMsg(proposed, rules);
-                error = errorMsg !== undefined;
-                if (error) {
-                    consoleMPDS.writeln(errorMsg);
-                }
-            } while (error);
-            return proposed;
+                const proposed = game.player.proposeCombination();
+                game.board.addAttempt(proposed);
+                game.board.show();
+            } while (!isEndGame(game));
+            showEndGameMsg(game);
+        }
+    };
+    return game;
 
-            function getErrorMsg(proposed, { COMBINATION_LENGTH, COLORS }) {
-                let errorMsg;
-                if (hasInvalidLength(proposed, COMBINATION_LENGTH)) {
-                    errorMsg = `Wrong proposed combination length`;
-                } else if (hasInvalidValues(proposed, COLORS)) {
-                    errorMsg = `Wrong colors, they must be ${arrayToString(COLORS)}`;
-                } else if (hasDuplicatedValues(proposed)) {
-                    errorMsg = `Wrong proposed combination, colors can't be duplicated`;
-                }
-                return errorMsg;
+    function showWelcome() {
+        consoleMPDS.writeln(`----- MASTERMIND -----`);
+    }
 
-                function hasInvalidLength(values, expectedLength) {
-                    return values.length !== expectedLength;
-                }
+    function generateSecretRandomly({ validator }) {
+        let secret = [];
 
-                function hasInvalidValues(values, validValues) {
-                    for (let value of values) {
-                        if (!isValuePresent(validValues, value)) {
-                            return true;
-                        }
-                    }
-                    return false;
-                }
+        for (let i = 0; i < validator.COMBINATION_LENGTH; i++) {
+            let color;
+            do {
+                let indexColor = parseInt(Math.random() * validator.COLORS.length);
+                color = validator.COLORS[indexColor];
+            } while (initArrayAdapter(secret).hasItem(color));
+            secret[i] = color;
+        }
+        return secret;
+    }
 
-                function arrayToString(array) {
-                    let msg = '';
-                    for (let value of array) {
-                        msg += value;
-                    }
-                    return msg;
-                }
+    function isEndGame({ board }) {
+        return board.isLastAttemptWinner() || board.hasMaxAttempts();
+    }
 
-                function hasDuplicatedValues(values) {
-                    let copy = [];
-                    for (let i = 0; i < values.length; i++) {
-                        if (isValuePresent(copy, values[i])) {
-                            return true;
-                        }
-                        copy[i] = values[i];
-                    }
-                    return false;
-                }
+    function showEndGameMsg({ board }) {
+        consoleMPDS.writeln(`You've ${board.isLastAttemptWinner() ? `won!!! ;-)` : `lost!!! :-(`}`);
+    }
+}
+
+function initBoard() {
+    const board = {
+        secret: [],
+        attempts: [],
+        MAX_ATTEMPTS: 10,
+
+        addAttempt: function (proposed) {
+            const attempt = initAttempt(proposed, board.secret);
+            initArrayAdapter(board.attempts).addAtEnd(attempt);
+        },
+
+        show: function () {
+            showAttemptsCount(board);
+            showSecret(board);
+            showAttempts(board);
+        },
+
+        isLastAttemptWinner: function () {
+            const attemptsAdapted = initArrayAdapter(board.attempts);
+            if (attemptsAdapted.hasLength()) {
+                return attemptsAdapted.getLast().isWinner();
             }
-        }
+            return false;
+        },
 
-        function addAttempt({ secret, attempts }, proposed) {
-            let blackCount = 0;
-            let whiteCount = 0;
-            for (let i = 0; i < proposed.length; i++) {
-                if (secret[i] === proposed[i]) {
-                    blackCount++;
-                } else if (isValuePresent(secret, proposed[i])) {
-                    whiteCount++;
-                }
-            }
-            attempts[attempts.length] = {
-                proposed: proposed,
-                blacks: blackCount,
-                whites: whiteCount
-            };
+        hasMaxAttempts: function () {
+            return initArrayAdapter(board.attempts).hasLength(board.MAX_ATTEMPTS);
         }
+    };
+    return board;
 
-        function isWinner({ attempts }) {
-            const lastAttempt = attempts[attempts.length - 1];
-            return lastAttempt.blacks === lastAttempt.proposed.length;
+    function showAttemptsCount({ attempts }) {
+        consoleMPDS.writeln(`\n${attempts.length} attempt(s):`)
+    }
+
+    function showSecret({ secret }) {
+        const HIDDEN_CHAR = '*';
+        let msg = '';
+        for (let i = 0; i < secret.length; i++) {
+            msg += HIDDEN_CHAR;
         }
+        consoleMPDS.writeln(msg);
+    }
 
-        function hasMoreAttempts({ board, rules }) {
-            return board.attempts.length < rules.MAX_ATTEMPTS;
+    function showAttempts({ attempts }) {
+        for (let i = 0; i < attempts.length; i++) {
+            attempts[i].show();
+        }
+    }
+}
+
+function initAttempt(proposed, secret) {
+    const attempt = {
+        proposed: proposed,
+        result: initResult(proposed, secret),
+
+        show: function () {
+            let msg = initArrayAdapter(attempt.proposed).toString();
+            msg += ` --> ${attempt.result.countBlacks()} blacks and ${attempt.result.countWhites()} whites`;
+            consoleMPDS.writeln(msg);
+        },
+
+        isWinner: function () {
+            return attempt.proposed.length === attempt.result.countBlacks();
+        }
+    };
+    return attempt;
+}
+
+function initResult(proposed, secret) {
+    const VALID_VALUES = ["black", "white", "fail"];
+    const result = {
+        VALID_VALUES: VALID_VALUES,
+        success: calculateSuccess(proposed, secret, VALID_VALUES),
+
+        countBlacks: function () {
+            return initArrayAdapter(result.success).count(getBlack(result));
+        },
+
+        countWhites: function () {
+            return initArrayAdapter(result.success).count(getWhite(result));
         }
     }
 
-    function isValuePresent(values, value) {
-        for (let i = 0; i < values.length; i++) {
-            if (value === values[i]) {
+    return result;
+
+    function calculateSuccess(proposed, secret, [black, white, fail]) {
+        const values = [];
+        for (let i = 0; i < proposed.length; i++) {
+            if (proposed[i] === secret[i]) {
+                values[i] = black;
+            } else if (initArrayAdapter(secret).hasItem(proposed[i])) {
+                values[i] = white;
+            } else {
+                values[i] = fail;
+            }
+        }
+        return values;
+    }
+
+    function getBlack(result) {
+        return result.VALID_VALUES[0];
+    }
+
+    function getWhite(result) {
+        return result.VALID_VALUES[1];
+    }
+
+}
+
+function initCombinationValidator() {
+    const validator = {
+        COMBINATION_LENGTH: 4,
+        COLORS: ['r', 'g', 'y', 'b', 'm', 'c'],
+        errorMsg: "",
+        OK: "ok",
+
+        validate: function (combination) {
+            validator.errorMsg = validator.OK;
+            if (hasInvalidLength(validator, combination)) {
+                validator.errorMsg = `Wrong proposed combination length`;
+            } else if (hasInvalidColors(validator, combination)) {
+                const colorsMsg = initArrayAdapter(validator.COLORS).toString();
+                validator.errorMsg = `Wrong colors, they must be ${colorsMsg}`;
+            } else if (hasDuplicatedValues(combination)) {
+                validator.errorMsg = `Wrong proposed combination, colors can't be duplicated`;
+            }
+        },
+
+        isValid: function () {
+            return validator.errorMsg === validator.OK;
+        },
+
+        showError: function () {
+            consoleMPDS.writeln(validator.errorMsg);
+        }
+    };
+    return validator;
+
+    function hasInvalidLength({ COMBINATION_LENGTH }, combination) {
+        return !initArrayAdapter(combination).hasLength(COMBINATION_LENGTH);
+    }
+
+    function hasInvalidColors({ COLORS }, combination) {
+        const colorsAdapted = initArrayAdapter(COLORS);
+        for (let color of combination) {
+            if (!colorsAdapted.hasItem(color)) {
                 return true;
             }
         }
         return false;
     }
 
-    function isResume() {
-        const VALID_ANSWERS = ['y', 'n'];
-        let answer;
-        let error;
-        do {
-            answer = consoleMPDS.readString(`Do you want to continue? (${VALID_ANSWERS[0]}/${VALID_ANSWERS[1]}): `);
-            error = !isValuePresent(VALID_ANSWERS, answer);
-            if (error) {
-                consoleMPDS.writeln(`Wrong response, it must be: '${VALID_ANSWERS[0]}' or '${VALID_ANSWERS[1]}'`)
+    function hasDuplicatedValues(combination) {
+        let copy = initArrayAdapter();
+        for (let i = 0; i < combination.length; i++) {
+            if (copy.hasItem(combination[i])) {
+                return true;
             }
-        } while (error);
-        return answer === VALID_ANSWERS[0];
+            copy.addAtEnd(combination[i]);
+        }
+        return false;
     }
 }
 
+function initPlayer(validator) {
+    const player = {
+        validator: validator,
 
+        proposeCombination: function () {
+            let proposed;
+            do {
+                let answer = consoleMPDS.readString(`Propose a combination:`);
+                proposed = initStringAdapter(answer).toArray();
+                player.validator.validate(proposed);
+                if (!player.validator.isValid()) {
+                    player.validator.showError();
+                }
+            } while (!player.validator.isValid());
+            return proposed;
+        }
+    };
+    return player;
+}
 
+function initYesNoDialog(question, VALID_ANSWERS = ["y", "n"]) {
+    const dialog = {
+        question: question,
+        answer: ``,
+        VALID_ANSWERS: VALID_ANSWERS,
+
+        ask: function () {
+            let error = false;
+            do {
+                dialog.answer = consoleMPDS.readString(`${dialog.question} (${getAffirmative(dialog)}/${getNegative(dialog)}):`);
+                error = !isValid(dialog);
+                if (error) {
+                    consoleMPDS.writeln(`Please, answer "${getAffirmative(dialog)}" or "${getNegative(dialog)}"`);
+                }
+            } while (error);
+        },
+
+        isAffirmative: function () {
+            return dialog.answer === getAffirmative(dialog);
+        }
+    };
+
+    return dialog;
+
+    function isValid(dialog) {
+        return initArrayAdapter(dialog.VALID_ANSWERS).hasItem(dialog.answer);
+    }
+
+    function getAffirmative(dialog) {
+        return dialog.VALID_ANSWERS[0];
+    }
+
+    function getNegative(dialog) {
+        return dialog.VALID_ANSWERS[1];
+    }
+}
+
+function initStringAdapter(text) {
+    const stringAdapter = {
+        text: text,
+
+        toArray: function () {
+            const array = [];
+            for (let i = 0; i < stringAdapter.text.length; i++) {
+                array[i] = stringAdapter.text[i];
+            }
+            return array;
+        }
+    };
+
+    return stringAdapter;
+}
+
+function initArrayAdapter(array = []) {
+    const adapted = {
+        array: array,
+
+        addAtEnd: function (item) {
+            adapted.array[adapted.array.length] = item;
+        },
+
+        getLast: function () {
+            return adapted.array[adapted.array.length - 1];
+        },
+
+        hasLength: function (expected) {
+            const length = adapted.array.length;
+            return expected !== undefined ? length === expected : length > 0;
+        },
+
+        hasItem: function (target) {
+            for (let item of adapted.array) {
+                if (item === target) {
+                    return true;
+                }
+            }
+            return false;
+        },
+
+        count: function (target) {
+            let count = 0;
+            for (let item of adapted.array) {
+                if (item === target) {
+                    count++;
+                }
+            }
+            return count;
+        },
+
+        toString: function () {
+            let text = '';
+            for (let item of adapted.array) {
+                text += item;
+            }
+            return text;
+        }
+    };
+
+    return adapted;
+}
 
