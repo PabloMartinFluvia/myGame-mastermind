964f4cb919aeb8d9814b96c8c448dc076394272d
Pablo Martin <paumf00@gmail.com>
2024-09-21 23:44:00 +0200

    review 1 js_v0.2.4

--- src/app.js
+++ src/app.js
@@ -7,351 +7,286 @@ playMastermind();
 function playMastermind() {
     const continueDialog = initYesNoDialog("Do you want to continue?");
     do {
-        const newGame = initGame();
-        newGame.play();
+        initGame().play();
         continueDialog.ask();
     } while (continueDialog.isAffirmative());
-}
 
-function initGame() {
-    const COMBINATION_LENGTH = 4;
-    const COLORS = ['r', 'g', 'y', 'b', 'm', 'c'];
-    const MAX_ATTEMPTS = 10;
-    const secret = generateSecretRandomly(COMBINATION_LENGTH, COLORS);
-    
-    const that = {
-        board: initBoard(secret, MAX_ATTEMPTS),
-        player: initPlayer(COMBINATION_LENGTH, COLORS),
+    function initGame() {
+        const that = {
+            secret: initSecretCombination(),
+            attempts: [],
+            MAX_ATTEMPTS: 10,
 
-        showWelcome: function () {
-            consoleMPDS.writeln(`----- MASTERMIND -----`);
-        },        
+            show: function () {
+                consoleMPDS.writeln(`\n${this.attempts.length} attempt(s):`);
+                this.secret.show();
+                for (let attempt of this.attempts) {
+                    attempt.show();
+                }
+            },
 
-        isEndGame: function () {
-            return this.board.isLastAttemptWinner() || this.board.hasMaxAttempts();
-        },
+            addProposed: function (proposed) {
+                const result = this.secret.getResult(proposed);
+                this.attempts[this.attempts.length] = initAttempt(proposed, result);
+            },
 
-        showEndGameMsg: function () {
-            consoleMPDS.writeln(`You've ${this.board.isLastAttemptWinner() ? `won!!! ;-)` : `lost!!! :-(`}`);
+            isEndGame: function () {
+                return this.isLastResultWinner() || this.attempts.length === this.MAX_ATTEMPTS;
+            },
+
+            isLastResultWinner: function () {
+                return this.attempts[this.attempts.length - 1].isResultWinner();
+            },
         }
-    };
 
-    return {
-        play: function () {
-            that.showWelcome();
-            that.board.show();
-            do {
-                const proposed = that.player.proposeCombination();
-                that.board.addAttempt(proposed);
-                that.board.show();
-            } while (!that.isEndGame());
-            that.showEndGameMsg();
-        }
-    };
-
-    function generateSecretRandomly(COMBINATION_LENGTH, COLORS) {
-        let secret = [];
-
-        for (let i = 0; i < COMBINATION_LENGTH; i++) {
-            let color;
-            do {
-                let indexColor = parseInt(Math.random() * COLORS.length);
-                color = COLORS[indexColor];
-            } while (initArrayAdapter(secret).hasItem(color));
-            secret[i] = color;
-        }
-        return secret;
-    }
-}
-
-function initBoard(secret, MAX_ATTEMPTS) {
-    const that = {
-        secret: secret,
-        MAX_ATTEMPTS: MAX_ATTEMPTS,
-        attempts: [],
-
-        showAttemptsCount: function () {
-            consoleMPDS.writeln(`\n${this.attempts.length} attempt(s):`)
-        },
-
-        showSecret: function () {
-            const HIDDEN_CHAR = '*';
-            let msg = '';
-            for (let i = 0; i < this.secret.length; i++) {
-                msg += HIDDEN_CHAR;
+        return {
+            play: function () {
+                consoleMPDS.writeln(`----- MASTERMIND -----`);
+                that.secret.setRandom();
+                that.show();
+                do {
+                    const proposed = initProposedCombination();
+                    proposed.ask();
+                    that.addProposed(proposed);
+                    that.show();
+                } while (!that.isEndGame());
+                consoleMPDS.writeln(`You've ${that.isLastResultWinner() ? `won!!! ;-)` : `lost!!! :-(`}`);
             }
-            consoleMPDS.writeln(msg);
-        },
+        };
 
-        showAttempts: function () {
-            for (let i = 0; i < this.attempts.length; i++) {
-                this.attempts[i].show();
-            }
+        function initSecretCombination() {
+            const that = {
+                combination: initCombination()
+            };
+
+            return {
+                setRandom: function () {
+                    for (let i = 0; i < that.combination.getCOMBINATION_LENGTH(); i++) {
+                        let color;
+                        do {
+                            const VALID_COLORS = that.combination.getVALID_COLORS();
+                            color = VALID_COLORS[parseInt(Math.random() * VALID_COLORS.length)];
+                        } while (that.combination.hasColor(color));
+                        that.combination.addColor(color);
+                    }
+                },
+
+                show: function () {
+                    const HIDDEN_CHAR = '*';
+                    let msg = '';
+                    for (let i = 0; i < that.combination.getLength(); i++) {
+                        msg += HIDDEN_CHAR;
+                    }
+                    consoleMPDS.writeln(msg);
+                },
+
+                getResult: function (proposed) {
+                    let blacks = 0;
+                    let whites = 0;
+                    for (let i = 0; i < that.combination.getLength(); i++) {
+                        if (that.combination.getColor(i) === proposed.getColor(i)) {
+                            blacks++;
+                        } else if (that.combination.hasColor(proposed.getColor(i))) {
+                            whites++;
+                        }
+                    }
+                    return initResult(blacks, whites, that.combination.getLength());
+                },
+            };
         }
-    }
 
-    return {
-        addAttempt: function (proposed) {
-            const attempt = initAttempt(proposed, initResult(proposed, that.secret));
-            initArrayAdapter(that.attempts).addAtEnd(attempt);
-        },
+        function initProposedCombination() {
+            const that = {
+                combination: undefined,
 
-        show: function () {
-            that.showAttemptsCount();
-            that.showSecret();
-            that.showAttempts();
-        },
+                getErrorMsg: function () {
+                    if (!this.combination.hasValidLength()) {
+                        return `Wrong proposed combination length`;
+                    } else if (!this.combination.hasValidColors()) {
+                        let errorMsg = `Wrong colors, they must be: `;
+                        for (let validColor of this.combination.getVALID_COLORS()) {
+                            errorMsg += validColor;
+                        }
+                        return errorMsg;
+                    } else if (this.combination.hasRepeatedColors()) {
+                        return `Wrong proposed combination, colors can't be repeated`;
+                    }
+                    return undefined;
+                }
+            };
 
-        isLastAttemptWinner: function () {
-            const attemptsAdapted = initArrayAdapter(that.attempts);
-            if (attemptsAdapted.hasLength()) {
-                return attemptsAdapted.getLast().isWinner();
-            }
-            return false;
-        },
+            return {
+                ask: function () {
+                    let errorMsg;
+                    do {
+                        that.combination = initCombination();
+                        let answer = consoleMPDS.readString(`Propose a combination:`);
+                        for (let i = 0; i < answer.length; i++) {
+                            that.combination.addColor(answer[i]);
+                        }
+                        errorMsg = that.getErrorMsg();
+                        if (errorMsg !== undefined) {
+                            consoleMPDS.writeln(errorMsg);
+                        }
+                    } while (errorMsg !== undefined);
+                },
 
-        hasMaxAttempts: function () {
-            return initArrayAdapter(that.attempts).hasLength(that.MAX_ATTEMPTS);
+                show: function () {
+                    let msg = '';
+                    for (let i = 0; i < that.combination.getLength(); i++) {
+                        msg += that.combination.getColor(i);
+                    }
+                    consoleMPDS.write(msg);
+                },
+
+                getColor: function (index) {
+                    return that.combination.getColor(index);
+                }
+            };
         }
-    };
-}
 
-function initAttempt(proposed, result) {
-    const that = {
-        proposed: proposed,
-        result: result
-    };
+        function initCombination() {
+            const that = {
+                COMBINATION_LENGTH: 4,
+                VALID_COLORS: ['r', 'g', 'y', 'b', 'm', 'c'],
+                colors: [],
+            };
 
-    return {
-        show: function () {
-            let msg = initArrayAdapter(that.proposed).toString();
-            msg += ` --> ${that.result.countBlacks()} blacks and ${that.result.countWhites()} whites`;
-            consoleMPDS.writeln(msg);
-        },
+            return {
+                addColor: function (color) {
+                    that.colors[that.colors.length] = color;
+                },
 
-        isWinner: function () {
-            return that.proposed.length === that.result.countBlacks();
-        }
-    };
-}
+                hasColor: function (colorTested) {
+                    for (let color of that.colors) {
+                        if (color === colorTested) {
+                            return true;
+                        }
+                    }
+                    return false;
+                },
 
-function initResult(proposed, secret) {
-    const VALID_VALUES = ["black", "white", "fail"];
-    const success = calculateSuccess(proposed, secret, VALID_VALUES);
+                hasValidLength: function () {
+                    return that.colors.length === that.COMBINATION_LENGTH;
+                },
 
-    const that = {
-        VALID_VALUES: VALID_VALUES,
-        success: success,
-
-        getBlack: function () {
-            return VALID_VALUES[0];
-        },
-
-        getWhite: function () {
-            return VALID_VALUES[1];
-        }
-    };
-
-    return {
-        countBlacks: function () {
-            return initArrayAdapter(that.success).count(that.getBlack());
-        },
-
-        countWhites: function () {
-            return initArrayAdapter(that.success).count(that.getWhite());
-        }
-    }
-
-    function calculateSuccess(proposed, secret, [black, white, fail]) {
-        const success = [];
-        for (let i = 0; i < proposed.length; i++) {
-            if (proposed[i] === secret[i]) {
-                success[i] = black;
-            } else if (initArrayAdapter(secret).hasItem(proposed[i])) {
-                success[i] = white;
-            } else {
-                success[i] = fail;
-            }
-        }
-        return success;
-    }
-}
-
-function initCombinationValidator(COMBINATION_LENGTH, COLORS) {
-    const that = {
-        COMBINATION_LENGTH: COMBINATION_LENGTH,
-        COLORS: COLORS,
-        OK: 'ok',
-        errorMsg: '',
-
-        hasInvalidLength: function (combination) {
-            return !initArrayAdapter(combination).hasLength(this.COMBINATION_LENGTH);
-        },
-
-        hasInvalidColors: function (combination) {
-            const colorsAdapted = initArrayAdapter(this.COLORS);
-            for (let color of combination) {
-                if (!colorsAdapted.hasItem(color)) {
+                hasValidColors: function () {
+                    for (let color of that.colors) {
+                        let found = false;
+                        for (let i = 0; !found && i < that.VALID_COLORS.length; i++) {
+                            found = color === that.VALID_COLORS[i];
+                        }
+                        if (!found) {
+                            return false;
+                        }
+                    }
                     return true;
-                }
-            }
-            return false;
-        },
+                },
 
-        hasDuplicatedValues: function (combination) {
-            let copy = initArrayAdapter([]);
-            for (let i = 0; i < combination.length; i++) {
-                if (copy.hasItem(combination[i])) {
-                    return true;
+                hasRepeatedColors: function () {
+                    for (let i = 0; i < that.colors.length - 1; i++) {
+                        for (let j = i + 1; j < that.colors.length; j++) {
+                            if (that.colors[i] === that.colors[j]) {
+                                return true;
+                            }
+                        }
+                    }
+                    return false;
+                },
+
+                getColor: function (index) {
+                    return that.colors[index];
+                },
+
+                getLength: function () {
+                    return that.colors.length;
+                },
+
+                getCOMBINATION_LENGTH: function () {
+                    return that.COMBINATION_LENGTH;
+                },
+
+                getVALID_COLORS: function () {
+                    return that.VALID_COLORS;
                 }
-                copy.addAtEnd(combination[i]);
+            };
+        }
+
+        function initAttempt(proposed, result) {
+            const that = {
+                proposed: proposed,
+                result: result
+            };
+
+            return {
+                show: function () {
+                    that.proposed.show();
+                    consoleMPDS.write(' --> ');
+                    that.result.show();
+                },
+
+                isResultWinner: function () {
+                    return that.result.isWinner();
+                }
+            };
+        }
+
+        function initResult(blacks, whites, secretLength) {
+            const that = {
+                blacks: blacks,
+                whites: whites,
+                secretLength: secretLength
             }
-            return false;
+
+            return {
+                isWinner: function () {
+                    return blacks === secretLength;
+                },
+
+                show: function () {
+                    consoleMPDS.writeln(`${that.blacks} blacks and ${that.whites} whites`);
+                }
+            };
         }
     }
-    return {
-        validate: function (combination) {
-            that.errorMsg = that.OK;
-            if (that.hasInvalidLength(combination)) {
-                that.errorMsg = `Wrong proposed combination length`;
-            } else if (that.hasInvalidColors(combination)) {
-                const colorsMsg = initArrayAdapter(that.COLORS).toString();
-                that.errorMsg = `Wrong colors, they must be ${colorsMsg}`;
-            } else if (that.hasDuplicatedValues(combination)) {
-                that.errorMsg = `Wrong proposed combination, colors can't be duplicated`;
+
+    function initYesNoDialog(question) {
+        const that = {
+            question: question,
+            VALID_ANSWERS: ["y", "n"],
+            answer: undefined,
+
+            isNegative: function () {
+                return this.answer === this.getNegative();
+            },
+
+            getAffirmative: function () {
+                return this.VALID_ANSWERS[0];
+            },
+
+            getNegative: function () {
+                return this.VALID_ANSWERS[1];
             }
-        },
+        };
 
-        isValid: function () {
-            return that.errorMsg === that.OK;
-        },
+        return {
+            ask: function () {
+                let error = false;
+                do {
+                    that.answer = consoleMPDS.readString(`${that.question} (${that.getAffirmative()}/${that.getNegative()}):`);
+                    error = !this.isAffirmative() && !that.isNegative();
+                    if (error) {
+                        consoleMPDS.writeln(`Please, answer "${that.getAffirmative()}" or "${that.getNegative()}"`);
+                    }
+                } while (error);
+            },
 
-        showError: function () {
-            consoleMPDS.writeln(that.errorMsg);
-        }
-    };
-
-
+            isAffirmative: function () {
+                return that.answer === that.getAffirmative();
+            }
+        };
+    }
 }
 
-function initPlayer(COMBINATION_LENGTH, COLORS) {
-    const that = {
-        validator: initCombinationValidator(COMBINATION_LENGTH, COLORS),
-    };
 
-    return {
-        proposeCombination: function () {
-            let proposed;
-            do {
-                let answer = consoleMPDS.readString(`Propose a combination:`);
-                proposed = initStringAdapter(answer).toArray();
-                that.validator.validate(proposed);
-                if (!that.validator.isValid()) {
-                    that.validator.showError();
-                }
-            } while (!that.validator.isValid());
-            return proposed;
-        }
-    };
-}
-
-function initYesNoDialog(question) {
-    const that = {
-        question: question,
-        VALID_ANSWERS: ["y", "n"],
-        answer: undefined,
-
-        isValid: function () {
-            return initArrayAdapter(this.VALID_ANSWERS).hasItem(this.answer);
-        },
-
-        getAffirmative: function () {
-            return this.VALID_ANSWERS[0];
-        },
-
-        getNegative: function () {
-            return this.VALID_ANSWERS[1];
-        }
-    };
-
-    return {
-        ask: function () {
-            let error = false;
-            do {
-                that.answer = consoleMPDS.readString(`${that.question} (${that.getAffirmative()}/${that.getNegative()}):`);
-                error = !that.isValid();
-                if (error) {
-                    consoleMPDS.writeln(`Please, answer "${that.getAffirmative()}" or "${that.getNegative()}"`);
-                }
-            } while (error);
-        },
-
-        isAffirmative: function () {
-            return that.answer === that.getAffirmative();
-        }
-    };
-}
-
-function initStringAdapter(text) {
-    const that = {
-        text: text
-    };
-    return {
-        toArray: function () {
-            const array = [];
-            for (let i = 0; i < that.text.length; i++) {
-                array[i] = that.text[i];
-            }
-            return array;
-        }
-    };
-}
-
-function initArrayAdapter(array) {
-    const that = {
-        array: array
-    };
-
-    return {
-        addAtEnd: function (item) {
-            that.array[that.array.length] = item;
-        },
-
-        getLast: function () {
-            return that.array[that.array.length - 1];
-        },
-
-        hasLength: function (expected) {
-            const length = that.array.length;
-            return expected !== undefined ? length === expected : length > 0;
-        },
-
-        hasItem: function (target) {
-            for (let item of that.array) {
-                if (item === target) {
-                    return true;
-                }
-            }
-            return false;
-        },
-
-        count: function (target) {
-            let count = 0;
-            for (let item of that.array) {
-                if (item === target) {
-                    count++;
-                }
-            }
-            return count;
-        },
-
-        toString: function () {
-            let text = '';
-            for (let item of that.array) {
-                text += item;
-            }
-            return text;
-        }
-    };
-}
 
